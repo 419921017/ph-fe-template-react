0e3fd72ea7b3effbdfbd4f249d04b37e4d300373
power_840 <liuyu19921017@gmail.com>
2021-08-11 14:56:04 +0800

    feat: depend

--- src/components/ComputedOne/index.tsx
+++ src/components/ComputedOne/index.tsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { FC, memo } from 'react'
 import './index.scss'
 import { add } from 'Utils/math'
+import { RouteComponentProps, withRouter } from 'react-router-dom'
 
-interface IProps {
+type PageDispatchProps = {
+  [propName: string]: any
+}
+
+type PageOwnProps = {
+  [propName: string]: any
+}
+
+type PageState = {
   a: number
   b: number
 }
 
-function ComputedOne(props: IProps) {
+type IProps = PageState & PageDispatchProps & PageOwnProps & RouteComponentProps
+
+const ComputedOne: FC<IProps> = (props) => {
   const { a, b } = props
   const sum = add(a, b)
 
   return <p className='computed-one'>{`Hi, I'm computed one, my sum is ${sum}.`}</p>
 }
 
-export default ComputedOne
+export default memo(withRouter(ComputedOne))
